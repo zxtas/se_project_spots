8f9e557ecbcd8769811e7f25eaa49f31d1d7af53
zxtas <rionn.alden@gmail.com>
2026-02-16 20:13:31 -0800

    part1

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 const EditProfileBtn = document.querySelector(".profile__edit-btn");
 const EditProfileModal = document.querySelector("#edit-profile-modal");
-const EditProfileCloseBtn = document.querySelector(".modal__close-btn");
+const EditProfileCloseBtn = EditProfileModal.querySelector(".modal__close-btn");
+const EditProfileNameInput = EditProfileModal.querySelector(
+  "#profile-name-input",
+);
+const EditProfileDescriptionInput = EditProfileModal.querySelector(
+  "#profile-description-input",
+);
 
 const NewPostBtn = document.querySelector(".profile__add-btn");
 const NewPostModal = document.querySelector("#new-post-modal");
-const NewPostCloseBtn = document.querySelector(".modal__close-btn");
+const NewPostCloseBtn = NewPostModal.querySelector(".modal__close-btn");
+
+const ProfileNameEl = document.querySelector(".profile__name");
+const ProfileDescriptionEl = document.querySelector(".profile__description");
 
 EditProfileBtn.addEventListener("click", function () {
+  EditProfileNameInput.value = ProfileNameEl.textContent;
+  EditProfileDescriptionInput.value = ProfileDescriptionEl.textContent;
   EditProfileModal.classList.add("modal_is-opened");
 });
 
